f2335b8f58c0e8813a245b2d343c3ca86968942b
wuuxxn <owen20050723@gmail.com>
2024-11-18 14:29:08 +0800

    1

--- app/src/main/java/com/example/game2d/MainActivity.kt
+++ app/src/main/java/com/example/game2d/MainActivity.kt
@@ -36,7 +36,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
+}  //
 
 @Preview(showBackground = true)
 @Composable
